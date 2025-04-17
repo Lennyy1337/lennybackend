74bef3c76847c826c53191c65eb72791a1bceb79
lennyy1337 <lennyygameplay@gmail.com>
2025-04-17 21:31:25 +0200

    fix2

--- src/init/s3.ts
+++ src/init/s3.ts
@@ -5,7 +5,6 @@ export const s3 = new Storage({
         accessKeyId: process.env.S3_ACCESS_KEY as string,
         secretAccessKey: process.env.S3_ACCESS_SECRET_KEY as string,
     },
-    forcePathStyle: true,
     region: process.env.S3_REGION as string,
     bucket: process.env.S3_BUCKET as string,
 });
